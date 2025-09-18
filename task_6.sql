12182ec10d0b70f30874624509fa099cb432e0b5
AKN <kamauanthony345@gmail.com>
2025-09-18 22:15:03 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -15,6 +15,13 @@ DB_CONFIG = {
 CUSTOMER_IDS_TO_CHECK = [2, 3, 4]
 SQL_FILE = "task_6.sql"
 
+    INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES 
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
+
+
 
 def check_sql_file(file_path):
     print(f"Checking SQL file: {file_path}")
